61e9246d28b4f849a832e6d82f1c53d7178e2e17
Fred Wang <namelos@outlook.com>
2016-03-08 15:57:53 +0800

    finish model$

--- src/index.js
+++ src/index.js
@@ -1,18 +1,60 @@
-import { run } from '@cycle/core'
-import { div, label, input, hr, h1, makeDOMDriver } from '@cycle/dom'
+import Rx from 'rxjs/Rx'
+const { Observable } = Rx
+const { of, from } = Observable
+const log = console.log.bind(console)
 
-const main = ({ DOM }) => ({
-  DOM: DOM.select('.field').events('input')
-    .map(ev => ev.target.value)
-    .startWith('')
-    .map(name =>
-      div([
-        label('Name:'),
-        input('.field', {attributes: {type: 'text'}}),
-        hr(),
-        h1('Hello ' + name),
-      ])
-    )
-})
+const data = [
+  'ITEM000001',
+  'ITEM000001',
+  'ITEM000001',
+  'ITEM000001',
+  'ITEM000001',
+  'ITEM000003-2',
+  'ITEM000005',
+  'ITEM000005',
+  'ITEM000005'
+]
 
-run(main, { DOM: makeDOMDriver(document.getElementById('app')) })
+const dict = {
+  ITEM000001: {
+    name: 'ball',
+    price: 1,
+    unit: ''
+  },
+  ITEM000003: {
+    name: 'apple',
+    price: 5.5,
+    unit: ''
+  },
+  ITEM000005: {
+    name: 'coke',
+    price: 3,
+    unit: ''
+  }
+}
+
+const parseString = item => {
+  const pair = item.split('-')
+  const category = pair[0]
+  const quantity = parseInt(pair[1] || 1)
+  return { category, quantity }
+}
+const categoryOfItem = item => item.category
+const sumQuantity = (x, y) => ({ ...x, quantity: x.quantity + y.quantity })
+const searchDict = item => ({ ...dict[item.category], ...item })
+const subtotal = item => ({ subtotal: item.price * item.quantity, ...item })
+
+const model$ = from(data)
+  .map(parseString)
+  .groupBy(categoryOfItem)
+  .flatMap(item$ => item$.reduce(sumQuantity))
+  .map(searchDict)
+  .map(subtotal)
+
+const subtotalOfItem = item => item.subtotal
+const total = (x, y) => x + y
+
+const total$ = model$
+  .map(subtotalOfItem)
+  .reduce(total)
+  .subscribe(log)
