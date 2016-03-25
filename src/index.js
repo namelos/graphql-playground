dc27a1c0e6f83ce8caf6f458e429860cfec20f68
Fred Wang <namelos@outlook.com>
2016-03-25 14:34:56 +0800

    finished component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { run } from '@cycle/core'
 import { makeDOMDriver, hJSX } from '@cycle/dom'
 import { makeHTTPDriver } from '@cycle/http'
+import isolate from '@cycle/isolate'
 import { Observable } from 'rx'
 const { of, just, combineLatest } = Observable
 
@@ -17,23 +18,28 @@ const LabeledSlider = ({ DOM, props$ }) => {
       <input type="range" className="slider" min={min} max={max} value={value} />
     </div>)
 
-  return { DOM, value$ }
+  return { DOM: vtree$, value$ }
 }
+
+const WeightSlider = isolate(LabeledSlider, 'weight')
+const HeightSlider = isolate(LabeledSlider, 'height')
 
 const main = ({ DOM }) => {
   const weightProps$ = of({ label: 'Weight', unit: 'kg', min: 40, max: 150, initial: 70 })
   const heightProps$ = of({ label: 'Height', unit: 'cm', min: 140, max: 210, initial: 170 })
 
-  const weightSlider = LabeledSlider({ DOM, props$: weightProps$ })
-  const heightSlider = LabeledSlider({ DOM, props$: heightProps$ })
+  const weightSlider = WeightSlider({ DOM, props$: weightProps$ })
+  const heightSlider = HeightSlider({ DOM, props$: heightProps$ })
 
   const bmi$ = combineLatest(weightSlider.value$, heightSlider.value$, (weight, height) =>
     Math.round(weight / (height * 0.01 * height * 0.01)))
 
   return {
     DOM: bmi$.combineLatest(weightSlider.DOM, heightSlider.DOM,
-      (bmi) => <div>
-        <h2></h2>
+      (bmi, weightVTree, heightVTree) => <div>
+        { weightVTree }
+        { heightVTree }
+        bmi { bmi }
       </div>
     )
   }
