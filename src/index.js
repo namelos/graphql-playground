55189c6bb6e1eae5ece59cb35adc89edd7adec77
Fred Wang <namelos@outlook.com>
2016-03-25 13:42:13 +0800

    add components

--- src/index.js
+++ src/index.js
@@ -2,19 +2,43 @@ import { run } from '@cycle/core'
 import { makeDOMDriver, hJSX } from '@cycle/dom'
 import { makeHTTPDriver } from '@cycle/http'
 import { Observable } from 'rx'
-const { of, just } = Observable
+const { of, just, combineLatest } = Observable
 
-run(({ DOM, HTTP }) => ({
-  DOM: HTTP.mergeAll().map(res => res.text).startWith('please type url:')
-    .map(text => <div>
-      <input type="text" className="input" />
-      <h1>response: { text }</h1>
-    </div>),
-  HTTP: DOM.select('.input').events('input').map(e => e.target.value)
-    .map(val => ({ url: val, method: 'GET' }))
-}), {
-  DOM: makeDOMDriver('#app'),
-  HTTP: makeHTTPDriver()
+const LabeledSlider = ({ DOM, props$ }) => {
+  const initialValue$ = props$.map(({ initial }) => initial).first()
+
+  const newValue$ = DOM.select('.slider').events('input').map(e => e.target.value)
+
+  const value$ = initialValue$.concat(newValue$)
+
+  const vtree$ = combineLatest(props$, value$,
+    ({ label, unit, min, max }, value) => <div className="label-slider">
+      <span className="label">{label} {value}{unit}</span>
+      <input type="range" className="slider" min={min} max={max} value={value} />
+    </div>)
+
+  return { DOM, value$ }
+}
+
+const main = ({ DOM }) => {
+  const weightProps$ = of({ label: 'Weight', unit: 'kg', min: 40, max: 150, initial: 70 })
+  const heightProps$ = of({ label: 'Height', unit: 'cm', min: 140, max: 210, initial: 170 })
+
+  const weightSlider = LabeledSlider({ DOM, props$: weightProps$ })
+  const heightSlider = LabeledSlider({ DOM, props$: heightProps$ })
+
+  const bmi$ = combineLatest(weightSlider.value$, heightSlider.value$, (weight, height) =>
+    Math.round(weight / (height * 0.01 * height * 0.01)))
+
+  return {
+    DOM: bmi$.combineLatest(weightSlider.DOM, heightSlider.DOM,
+      (bmi) => <div>
+        <h2>1</h2>
+      </div>
+    )
+  }
+}
+
+run(main, {
+  DOM: makeDOMDriver('#app')
 })
-
-
